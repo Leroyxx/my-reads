a101c8d851b8287936463cce9c7c9c81e7bb8f1c
Leroy Haimoff <leroyblergh@gmail.com>
2018-05-30 23:28:23 +0300

    feat: Dynamic rendering of books and shelves
    The Shelves state is passed to Bookshelves as props and the book data
    ispassed to Book as its own props. The props are mapped on with
    JSarray.map() for shelf creation and book creation.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Search from './Search'
-import Shelves from './Shelves'
+import Main from './Main'
+
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
-
   render() {
     return (
       <BrowserRouter>
       <div className="app">
         <Route exact path="/search" component={Search} />
-        <Route exact path="/" component={Shelves} />
+        <Route exact path="/" component={Main} />
       </div>
     </BrowserRouter>
     )
---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Shelves from './Shelves'
+
+class Main extends React.Component {
+  render() {
+    return <div className="list-books">
+      <div className="list-books-title"> <h1>MyReads</h1>  </div>
+      <Shelves />
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  }
+}
+
+export default Main
