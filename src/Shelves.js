5348e7a0f926ccb01a371d481f668db61f5303a7
Leroy Haimoff <leroyblergh@gmail.com>
2018-06-03 17:19:41 +0300

    feat: BookShelfChanger component will disable the current bookshelf option of each book

--- src/Shelves.js
+++ src/Shelves.js
@@ -5,48 +5,60 @@ import { Link } from 'react-router-dom'
 class Shelves extends React.Component {
   state = {
     shelvesArray: [
-      {
-        title: 'Currently Reading',
-        books: [
-          {
-            title: 'Loading Books',
-            authors: '',
-            imageLinks: {
-            thumbnail: ''
+        {
+          title: 'Currently Reading',
+          books: [
+            {
+              title: 'Loading Books',
+              authors: '',
+              authorsC: '', //Authors with commas between them
+              imageLinks: {
+              thumbnail: ''
+            }
           }
+        ]
+      }
+    ]
+  }
+
+  sortResponse(response) {
+    //Iterate over the books in the response
+    let shelvesUndone = response.reduce(
+      (object, book) => {
+        //If the shelf of the book is not listed as a key in the
+        //new object being created, create a shelf inside the object with the
+        //shelf name as a key and assign it a value of an empty books array
+        if (!object[book.shelf]) {
+          object[book.shelf] = [];
         }
-      ]
+        //then push the books into the array
+        object[book.shelf].push(book)
+        return object
+      }, {} //Initial value is an empty object
+    );
+
+    const camel2title = (camelCase) => camelCase
+    .replace(/([A-Z])/g, (match) => ` ${match}`)
+    .replace(/^./, (match) => match.toUpperCase());
+    //function by renevanderark on stackoverflow.com
+
+    let shelvesArray = [];
+    Object.keys(shelvesUndone).forEach(shelf => shelvesArray.push({title: camel2title(shelf), untitled: shelf}));
+    shelvesArray.forEach(shelf => shelf.books = shelvesUndone[shelf.untitled]);
+    shelvesArray.forEach(shelf => shelf.books.forEach(book => {
+      book.authorsC = book.authors.reduce((arr, author, index, array) => {
+        if( index !== array.length-1) { return [...arr, author, ", "] }
+        else { return [...arr, author] }  }, []);
+        //Add commas between authors and call this array differently (authorsC)
+      } ) )
+
+      return shelvesArray
     }
-  ]
-}
 
   componentDidMount() {
     BooksAPI.getAll()
     .then(response => {
-      let shelvesUndone = response.reduce(
-        (object, book) => {
-          if (!object[book.shelf]) {
-            object[book.shelf] = [];
-          }
-          object[book.shelf].push(book)
-          return object
-        }, {} //initial value is an empty object
-      )
-
-      const camel2title = (camelCase) => camelCase
-      .replace(/([A-Z])/g, (match) => ` ${match}`)
-      .replace(/^./, (match) => match.toUpperCase());
-      //function by renevanderark on stackoverflow.com
-
-      let shelvesArray = [];
-      Object.keys(shelvesUndone).forEach(shelf => shelvesArray.push({title: camel2title(shelf), untitled: shelf}));
-      shelvesArray.forEach(shelf => shelf.books = shelvesUndone[shelf.untitled]);
-      shelvesArray.forEach(shelf => shelf.books.forEach(book => {
-        book.authors = book.authors.reduce((arr, author, index, array) => { if( index !== array.length-1) { return [...arr, author, ", "] }
-        else { return [...arr, author] }  }, []);
-        //add commas between authors
-        } ) )
-
+      let shelvesArray = this.sortResponse(response);
       this.setState(  { shelvesArray: shelvesArray },
         () => {
           console.log(this.state) }
@@ -54,15 +66,16 @@ class Shelves extends React.Component {
       } )
   }
 
-  updateBooks(book, shelf) {
-    BooksAPI.update(book, shelf).then(
-      response => this.setState( {} )
-    )
+  moveBook(book, shelf) {
+    BooksAPI.update(book, shelf).then(response => {
+      let shelvesArray = this.sortResponse(response);
+      this.setState( {shelvesArray: shelvesArray} )
+    } )
   }
 
   render() {
     return this.state.shelvesArray.map(shelf => (<div className="list-books-content" key={shelf.title.toLowerCase().split(' ').join('-')}>
-        <BookShelf title={shelf.title} books={shelf.books}/>
+        <BookShelf shelf={shelf} shelvesArray={this.state.shelvesArray}/>
       </div>));
 }
 }
@@ -70,9 +83,9 @@ class Shelves extends React.Component {
 class BookShelf extends React.Component {
   render() {
     return <div className="bookshelf">
-      <h2 className="bookshelf-title">{this.props.title}</h2>
+      <h2 className="bookshelf-title">{this.props.shelf.title}</h2>
       <ol className="books-grid">
-        {this.props.books.map(book => <Book book={book} key={book.title.toLowerCase().split(' ').join('-')}/>)}
+        {this.props.shelf.books.map(book => <Book book={book} shelvesArray={this.props.shelvesArray} key={book.title.toLowerCase().split(' ').join('-')}/>)}
       </ol>
     </div>
   }
@@ -84,20 +97,28 @@ class Book extends React.Component {
               <div className="book">
                 <div className="book-top">
                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail })` }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
+                  <BookShelfChanger book={this.props.book} shelvesArray={this.props.shelvesArray} shelf={this.props.book}/>
                 </div>
                 <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors}</div>
+                <div className="book-authors">{this.props.book.authorsC}</div>
               </div>
             </li>
+  }
+}
+
+class BookShelfChanger extends React.Component {
+  render() {
+    return <div className="book-shelf-changer">
+      <select>
+      <option value="none" disabled>Move to...</option>
+      {this.props.shelvesArray.map(shelf => {
+        if (shelf.untitled === this.props.book.shelf) {
+        return <option disabled selected value={shelf.untitled}>{shelf.title}</option> }
+        return <option value={shelf.untitled}>{shelf.title}</option>
+      }) }
+      <option value="none">None</option>
+      </select>
+      </div>
   }
 }
 export default Shelves
